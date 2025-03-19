b38fd86db69eaed8cf5976f6e0e85f7c08159f0b
NatalieChurch <nataliechurch12@gmail.com>
2025-03-18 21:01:55 -0500

    Beginning Set Up of Workshop

---
+++ index.js
@@ -0,0 +1,32 @@
+// Your friend is an event organizer and has asked you to build a website for them. They want to be able to keep track of all the parties they are organizing. They want to be able to see a list of all the parties, add new parties, and delete parties.
+
+
+// A user enters the website and finds a list of the names, dates, times, locations, and descriptions of all the parties that are happening.
+// Next to each party in the list is a delete button. The user clicks the delete button for one of the parties. That party is then removed from the list.
+// There is also a form that allows the user to enter information about a new party that they want to schedule. After filling out the form and submitting it, the user observes their party added to the list of parties.
+
+
+// If you are stuck, try answering the following questions:
+
+    // 1) Which components can be created directly in the HTML? Which components need to be created in JavaScript?
+    // 2) Can you render mock data to the page?
+    // 3) Can you render real data to the page?
+    // 4) Are you able to fetch an array of all the parties from the API?
+    // 5) Is state correctly updated to match the data from the API?
+    // 6) Are you passing the correct arguments to fetch?
+    // 7) Does the API return an error? If so, what is the error message?
+    // 8) Is there an event listener on the form? Does it correctly add a new party to the list of parties?
+    // 9) Is there an event listener attached to each delete button? Does it correctly remove a party from the list of parties?
+
+
+// The URL to access a resource in the API is structured as follows:
+
+    // The base URL is https://fsa-crud-2aa9294fe819.herokuapp.com/api.
+    // The next segment is your cohort code. e.g. /2109-CPU-RM-WEB-PT
+    // The last segment is based on the resource you want to access. e.g. /recipes
+// For example, the URL for all recipes for the 2109 cohort would be:
+    // https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/recipes
+
+
+
+    
